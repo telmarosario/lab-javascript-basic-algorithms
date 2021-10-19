415784aeb09c49988f88ad227f2af5743375a759
Telma Rosário <telmarrosario@gmail.com>
2021-10-19 09:05:46 +0100

    loops and bonus 2

--- js/index.js
+++ js/index.js
@@ -17,12 +17,32 @@ if (hacker1.length > hacker2.length){
 
 // Iteration 3: Loops
 
+let nameSeparated = "";
 
-let separateAndCapitalize = hacker1.toUpperCase().split('').join(' ');
-console.log(separateAndCapitalize);
+for (let i = 0; i < hacker1.length; i ++){
+    nameSeparated += hacker1[i] + " ";
+
+}
+
+let capitalize = nameSeparated.toUpperCase();
+
+ console.log(capitalize);
+
+let reverseName = "";
+
+for (let i = 0; i <= hacker2.length; i++){
+    reverseName += hacker2.charAt(hacker2.length -[i]);
+}
+
+console.log(reverseName);
+
+ // Exercice 3 with methods
 
 let reverse = hacker2.split('').reverse().join('');
 console.log(reverse)
+
+let separateAndCapitalize = hacker1.toUpperCase().split('').join(' ');
+console.log(separateAndCapitalize);
     
 if (hacker1 < hacker2){
     console.log("The driver's name goes first");
@@ -36,15 +56,30 @@ if (hacker1 < hacker2){
 
 let paragraphs = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.  It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
 
-var count = 0;
+let count = 0;
+let countEt = 0; 
 
 for (let i = 0; i < paragraphs.length; i++){
     if (paragraphs[i] === " "){
         count += 1;
     }
+   if ((paragraphs[i] === "e") && (paragraphs[i + 1] === "t")){
+       countEt+= 1;
+   }
 }
 
-console.log(count)
+console.log(count);
+console.log(countEt);
+
+// Bonus 2
+
+let phraseToCheck = "Amor, Roma";
 
 
+for (let i = 0; i <= phraseToCheck.length; i++){
+    if (phraseToCheck[i] === (phraseToCheck.charAt(phraseToCheck.length -[i]))){
+        console.log("You have a Palindrome")
+    }
+    
+}
 
